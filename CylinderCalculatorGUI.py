93fc7c5680adeb5093d388646efcda3ebb338919
Jamieson Power <jamieson.power@JP.local>
2018-09-25 15:49:03 -0400

    CylinderCalculator Update

--- CylinderCalculatorGUI.py
+++ CylinderCalculatorGUI.py
@@ -1,5 +1,18 @@
 import tkinter as tk
+import math
 
+def submit():
+
+	print("Submit pressed")
+	r = float(entr.get())
+	h = float(enth.get())
+
+	v = math.pi*r*r*h
+	v = round(v,3)
+
+	output.config(state="normal")
+	output.insert(tk.INSERT,v)
+	output.config(state="disabled")
 
 root = tk.Tk()
 root.title("Volume of a Cylinder")
@@ -7,7 +20,7 @@ root.title("Volume of a Cylinder")
 labr = tk.Label(root, text="radius") #stage 1 create widget or element
 labr.pack() #Stage 3: pack the widget 
 
-entr =  tk.Entry(root)
+entr =  tk.Entry(root) 
 entr.pack()
 
 labh = tk.Label(root, text="height")
@@ -16,7 +29,7 @@ labh.pack()
 enth =  tk.Entry(root)
 enth.pack()
 
-btn = tk.Button(root, text="Submit")
+btn = tk.Button(root, text="Submit", command=submit)
 btn.pack()
 
 output = tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
